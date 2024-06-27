c0f9036ab2897800cdceb10ea618aa9f3ecebadc
RiveraGisela <giselarivera1812@gmail.com>
2024-06-27 16:55:36 -0500

    mejoras

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 
-
-// Variables para las puntuaciones
 let userScore = 0;
 let cpuScore = 0;
 
-// Función para jugar una ronda
+
 function play(userChoice) {
     if (userScore >= 3 || cpuScore >= 3) {
-        return; // Detener el juego si alguien ya alcanzó 3 puntos
+        return;
     }
 
-    const choices = ['CPU eligio piedra', 'CPU eligio papel', 'CPU eligio tijera'];
+    const choices = ['piedra', 'papel', 'tijera'];
     const cpuChoice = choices[Math.floor(Math.random() * choices.length)];
 
-    // Mostrar elecciones
-    document.getElementById('user-choice').textContent = userChoice;
-    document.getElementById('cpu-choice').textContent = cpuChoice;
 
-    // Determinar el ganador de la ronda
+    document.getElementById('user-choice').textContent = `Usuario eligió ${userChoice}`;
+    document.getElementById('cpu-choice').textContent = `CPU eligió ${cpuChoice}`;
+
+
     let roundResult = '';
     if (userChoice === cpuChoice) {
         roundResult = 'Empate';
@@ -33,23 +31,19 @@ function play(userChoice) {
         roundResult = 'La CPU ganó esta ronda';
     }
 
-    // Actualizar puntuaciones
     document.getElementById('user-score').textContent = userScore;
     document.getElementById('cpu-score').textContent = cpuScore;
-
-    // Mostrar el resultado de la ronda
     document.getElementById('round-result').textContent = roundResult;
 
-    // Verificar si alguien alcanzó 3 puntos
+    
     if (userScore === 3) {
-        //alert('¡Felicidades! ¡Ganaste el juego!');
         Swal.fire({
             title: "¡Felicidades! ¡Ganaste el juego! Preciona Reload para jugar de nuevo",
             imageUrl: './assets/game_over.jpg',
             imageWidth: 400,
             imageHeight: 200,
             imageAlt: "Custom image"
-            });
+        });
         disableButtons();
     } else if (cpuScore === 3) {
         Swal.fire({
@@ -58,20 +52,14 @@ function play(userChoice) {
             imageWidth: 400,
             imageHeight: 200,
             imageAlt: "Custom image"
-            });
-    
-       // alert('La CPU ha ganado el juego. ¡Inténtalo de nuevo!');
+        });
         disableButtons();
     }
 }
 
-// Función para deshabilitar los botones de elección
 function disableButtons() {
     const buttons = document.querySelectorAll('.user .buttons button');
     buttons.forEach(button => {
         button.disabled = true;
     });
 }
-
-
-
